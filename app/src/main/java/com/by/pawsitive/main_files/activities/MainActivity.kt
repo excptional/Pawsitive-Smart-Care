7ac8d5e5a15267a12a23536dada3bca612ce7aef
Bikash Dutta <bd27official@gmail.com>
2024-03-17 01:35:40 +0530

    Initial commit

---
+++ app/src/main/java/com/by/pawsitive/main_files/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.by.pawsitive.main_files.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.by.pawsitive.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

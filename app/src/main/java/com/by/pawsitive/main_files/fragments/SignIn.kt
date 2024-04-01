93d9588c3e411a2fdbc25514d1790b4d9706bd2d
Prince <banerjee.prince69@gmail.com>
2024-04-01 22:01:20 +0530

    Register Fragment Complete

--- app/src/main/java/com/by/pawsitive/main_files/fragments/SignIn.kt
+++ app/src/main/java/com/by/pawsitive/main_files/fragments/SignIn.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.findNavController
 import com.by.pawsitive.R
 
 class SignIn : Fragment() {
@@ -14,6 +16,11 @@ class SignIn : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_sign_in, container, false)
+        val registerTV: TextView = view.findViewById(R.id.register)
+        registerTV.setOnClickListener {
+            val navController = view.findNavController()
+            navController.navigate(R.id.nav_register1)
+        }
 
         return view
     }
